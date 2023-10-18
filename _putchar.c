643218e1e8f9f35db8fb22e2e360ea27e845b084
Buchiken11 <buchiken025@gmail.com>
2023-10-17 17:49:43 -0700

    updating 2

---
+++ _putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _putchar-function prints to the console standad output
+ * @c: function parameter of char.
+ * Return: 1 on success
+ */
+
+int _putchar(char c)
+{
+	write(1, &c, 1);
+	return (1);
+}
